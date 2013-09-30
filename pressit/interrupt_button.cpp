163c4946b5518486f3efb207e6f838a0d56229f6
Brian Dunn <brian+briandunn@theophil.us>
2013-09-29 21:41:11 -0500

    Reliably detect presses.

--- pressit/interrupt_button.cpp
+++ pressit/interrupt_button.cpp
@@ -4,13 +4,21 @@ volatile int
 InterruptButton::changes[] = {0, 0};
 
 InterruptButton::InterruptButton(int num) {
-	reportedChanges = digitalRead(num + 2);
+	reportedChanges = 0;
 	interuptNumber = num;
 	changes[num] = reportedChanges;
 	attachInterrupt(num, num == 0 ? change0 : change1, CHANGE);
 }
 
 /* the button has been pressed since the last time we asked */
+ /* if the button starts down, the changes sequence will be something like */
+ /* 0 ^ 1 v 2 ^ 3 v 4 */
+ /* if the button starts up, it looks like */
+ /*     1 v 2 ^ 3 v 4 */
+
+ /* so every even number represents the button in the downward position. */
+ /* so the answer to wasPressed: does the difference between the reported Changes and the changes span any even numbers? */
+
 bool
 InterruptButton::wasPressed() {
 	bool unreportedChanges = false;
@@ -21,12 +29,24 @@ InterruptButton::wasPressed() {
 	return unreportedChanges;
 }
 
-void
-InterruptButton::change0() {
-	changes[0]++;
+bool changed(int value) {
+	static int lastValue = -1;
+	bool c = false;
+	if(value != lastValue)
+		c = true;
+	lastValue = value;
+	return c;
 }
 
 void
+InterruptButton::change0() {
+	if(changed(digitalRead(2)))
+		changes[0] ++;
+	Serial.println(changes[0]);
+}
+void
 InterruptButton::change1() {
-	changes[1]++;
+	if(changed(digitalRead(3)))
+		changes[1] ++;
+	Serial.println(changes[1]);
 }
