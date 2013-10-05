c46fe683d548990b5759b18290b4d12a6d85c9ac
Brian Dunn <brian+briandunn@theophil.us>
2013-10-05 16:41:00 -0500

    wip

--- pressit/pressit.cpp
+++ pressit/pressit.cpp
@@ -1,56 +1,13 @@
 // Be the first to press your button when the song ends!
-#include "round.h"
 #include "pin.h"
-#include "interrupt_button.h"
 #include "song.h"
 
-Pin* yellow;
-Pin* piezo;
-Pin* red;
-const int roundCount = 3;
-Round* rounds[roundCount];
+Song* song;
 
 void setup() {
-	red                  = new Pin(10, OUTPUT);
-	piezo                = new Pin(11, OUTPUT);
-	yellow               = new Pin(13, OUTPUT);
-	Button* redButton    = new InterruptButton(0);
-	Button* yellowButton = new InterruptButton(1);
-	Song* song           = new Song(piezo, millis);
-	int i = 0;
-	while(i < roundCount)
-		rounds[i++] = new Round(song, redButton, yellowButton);
-	Serial.begin(9600);
-}
-
-int redWins() {
-	int wins = 0;
-	for(int i = 0; i < roundCount; i++)
-		if(rounds[i]->redWon()) wins++;
-
-	return wins;
-}
-
-void roundWon(Pin* ledPin) {
-	ledPin->digitalWrite(HIGH);
-}
-
-void gameWon(Pin* ledPin) {
-	ledPin->digitalWrite(HIGH);
+	Song* song = new Song(new Pin(11, OUTPUT), millis);
 }
 
 void loop() {
-	static bool gameOver   = false;
-	static int roundNumber = 1;
-	Round* round           = rounds[roundNumber - 1];
-	if(round->tick() && !gameOver) {
-		roundWon(round->redWon() ? red : yellow);
-		if(roundNumber == roundCount) { // game over
-			gameOver = true;
-			if(redWins() > roundCount / 2.0)
-				gameWon(red);
-			else
-				gameWon(yellow);
-		} else roundNumber++; // move on to the next round
-	}
+	song->play();
 }
