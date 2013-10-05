36b7017ef18393f0e4ff98e6cc3229e90e91816d
Brian Dunn <brian+briandunn@theophil.us>
2013-10-05 16:37:15 -0500

    Add missing duration specification to pin

--- pressit/pin.cpp
+++ pressit/pin.cpp
@@ -9,4 +9,5 @@ Pin::Pin(int num, int mode) {
 int  Pin::digitalRead()         { return ::digitalRead(num); }
 void Pin::digitalWrite(int val) { ::digitalWrite(num, val); }
 void Pin::tone(int val)         { ::tone(num, val); }
+void Pin::tone(int val, int dur){ ::tone(num, val, dur); }
 void Pin::noTone()              { ::noTone(num); }
