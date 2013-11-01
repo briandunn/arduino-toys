1866de4a94e65992220304f107b654c0083b7480
Brian Dunn and Derek Parker <dev+briandunn+dparker1990@hashrocket.com>
2013-11-01 15:36:26 -0500

    fix whitespace

--- animation_que_demo/demo.cpp
+++ animation_que_demo/demo.cpp
@@ -6,43 +6,43 @@ Pin* redLamp = new Pin(10, INPUT);
 Pin* yellowLamp = new Pin(9, INPUT);
 
 class SawTooth : public Animation {
-				Pin* pin;
-				int brightness;
-				int direction;
-				unsigned long elapsed;
-				public:
-				SawTooth(Pin* pin) : pin(pin), brightness(0), direction(1), elapsed(0) {}
-				void loop() {
-								unsigned long time = millis();
-								if(time - elapsed > 1) {
-									elapsed = time;
-									pin->analogWrite(brightness);
-									brightness += direction;
-									if (brightness % 255 == 0) direction *= -1;
-								}
-				}
+	Pin* pin;
+	int brightness;
+	int direction;
+	unsigned long elapsed;
+	public:
+	SawTooth(Pin* pin) : pin(pin), brightness(0), direction(1), elapsed(0) {}
+	void loop() {
+		unsigned long time = millis();
+		if(time - elapsed > 1) {
+			elapsed = time;
+			pin->analogWrite(brightness);
+			brightness += direction;
+			if (brightness % 255 == 0) direction *= -1;
+		}
+	}
 };
 
 class Strobe : public Animation {
-				Pin* pin;
-				int brightness;
-				unsigned long elapsed;
-				public:
-				Strobe(Pin* pin) : pin(pin), brightness(0), elapsed(0) {}
-				void loop() {
-								unsigned long time = millis();
-								if(time - elapsed > 100) {
-									elapsed = time;
-									pin->analogWrite(brightness);
-									brightness = brightness == 0 ? 255 : 0;
-								}
-				}
+	Pin* pin;
+	int brightness;
+	unsigned long elapsed;
+	public:
+	Strobe(Pin* pin) : pin(pin), brightness(0), elapsed(0) {}
+	void loop() {
+		unsigned long time = millis();
+		if(time - elapsed > 100) {
+			elapsed = time;
+			pin->analogWrite(brightness);
+			brightness = brightness == 0 ? 255 : 0;
+		}
+	}
 };
 
 void setup() {
-				AnimationQueue::add(new SawTooth(redLamp));
-				AnimationQueue::add(new Strobe(yellowLamp));
+	AnimationQueue::add(new SawTooth(redLamp));
+	AnimationQueue::add(new Strobe(yellowLamp));
 }
 void loop() {
-				AnimationQueue::loop();
+	AnimationQueue::loop();
 }
